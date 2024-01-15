cafdabc0ae3011d9b2fd4cfba2290d24e25d3114
reskeee <comandos200809@gmail.com>
2024-01-15 15:27:17 +0700

    snd commit

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
 import pygame
+from help import load_image, terminate
+from objects import Ground
 
+
+def start_screen():
+    intro_text = ['Свет и Тьма',
+                  'Жители Светогорода жили припеваючи',
+                  "пока злостные отряды тьмы не захватили",
+                  "его. Теперь в городе темно, а вам предстоит",
+                  "разобраться с захватчиками!"]
+
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+# инициализация окна
+pygame.init()
+size = HEIGHT, WIDTH = 800, 450
+screen = pygame.display.set_mode(size)
+screen.fill((38, 23, 82))
+FPS = 30
+clock = pygame.time.Clock()
+
+# загрузка изображений
+ground_img = load_image('ground.png')
+
+# запуск игры
 if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 800, 400
-    screen = pygame.display.set_mode(size)
-    pos = (width / 2, height / 2)
-
     running = True
-    x_pos = 0
-    v = 20  # пикселей в секунду
-    fps = 75
-    clock = pygame.time.Clock()
+    # Создание групп спрайтов
+    all_sprites = pygame.sprite.Group()
+    ground_group = pygame.sprite.Group()
+
+    # Создание спрайтов
+    Ground(ground_img, ground_group, all_sprites)  # Земля
+
+    # Запуск стартового окна
+    start_screen()
+    screen.fill((38, 23, 82))
+    # Запуск основного игрового цикла
     while running:
-        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEMOTION:
-                pos = event.pos
-            pygame.draw.circle(screen, (0, 0, 255), pos, 20)
+        all_sprites.draw(screen)
         pygame.display.flip()
-        clock.tick(50)
-    pygame.quit()
---
+++ objects.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, image, *group):
+        super().__init__(*group)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 385
+
+
+
